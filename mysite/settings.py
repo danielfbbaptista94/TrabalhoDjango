05bdc34d11a13ec5465b2ee73e746cba4283def5
Daniel Baptista <daniel94.baptista@gmail.com>
2018-10-10 19:34:58 -0300

    commit2

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'multiselectfield',
     'crispy_forms',
+    'django_tables2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
